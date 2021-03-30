270418cb10a8c45fd6108e5b943c9d433af50e3d
DanasPirmasis <alisauskas53@gmail.com>
2021-03-30 14:16:30 +0300

    init commit

---
+++ models/User.js
@@ -0,0 +1,64 @@
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const UserSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		required: [true, 'Please provide a username'],
+	},
+	email: {
+		type: String,
+		required: [true, 'Please provide an email'],
+		unique: true,
+		match: [
+			/^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/,
+			'Please provide a valid email',
+		],
+	},
+	password: {
+		type: String,
+		required: [true, 'Please enter a password'],
+		minlength: 6,
+		select: false,
+	},
+	resetPasswordToken: String,
+	resetPaswordExpire: Date,
+});
+
+UserSchema.pre('save', async function (next) {
+	if (!this.isModified('password')) {
+		next();
+	}
+
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt);
+	next();
+});
+
+UserSchema.methods.matchPassword = async function (password) {
+	return await bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.getSignedToken = function () {
+	return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+		expiresIn: process.env.JWT_EXPIRE,
+	});
+};
+
+UserSchema.methods.getResetPasswordToken = function () {
+	const resetToken = crypto.randomBytes(20).toString('hex');
+
+	this.resetPasswordToken = crypto
+		.createHash('sha256')
+		.update(resetToken)
+		.digest('hex');
+
+	this.resetPaswordExpire = Date.now() + 10 * (60 * 1000);
+	return resetToken;
+};
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
