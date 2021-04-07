cc918737bd20d8f75a72a49063f746f4afb57d98
DanasPirmasis <alisauskas53@gmail.com>
2021-04-07 19:25:53 +0300

    Added chatID display

--- client/src/components/screens/PrivateScreen.js
+++ client/src/components/screens/PrivateScreen.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import Modal from 'react-modal'
+import Modal from 'react-modal';
 
 Modal.setAppElement('#root');
 
@@ -8,6 +8,7 @@ const PrivateScreen = ({ history }) => {
 	const [error, setError] = useState('');
 	const [privateData, setPrivateData] = useState('');
 	const [modalOpen, setModalOpen] = useState(false);
+	const [chatIDS, setChatIDS] = useState('');
 
 	useEffect(() => {
 		if (!localStorage.getItem('authToken')) {
@@ -24,6 +25,7 @@ const PrivateScreen = ({ history }) => {
 			try {
 				const { data } = await axios.get('/api/private', config);
 				setPrivateData(data.data);
+				setChatIDS(data.chatIDS);
 			} catch (error) {
 				localStorage.removeItem('authToken');
 				setError('You are not authorized please login');
@@ -43,8 +45,9 @@ const PrivateScreen = ({ history }) => {
 	) : (
 		<>
 			<div style={{ background: 'green', color: 'white' }}>{privateData}</div>
+			<div>{chatIDS}</div>
 			<button onClick={logoutHandler}>Logout</button>
-			
+
 			<br />
 			<button onClick={() => setModalOpen(true)}>Open new message</button>
 			<Modal isOpen={modalOpen} onRequestClose={() => setModalOpen(false)}>
