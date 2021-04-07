656a7a1d44e461b632bf1fa3330dfde8d7a3d625
DanasPirmasis <alisauskas53@gmail.com>
2021-04-07 18:58:52 +0300

    Model update

--- models/User.js
+++ models/User.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken');
 const UserSchema = new mongoose.Schema({
 	username: {
 		type: String,
+		max: 32,
 		required: [true, 'Please provide a username'],
 	},
 	email: {
@@ -16,15 +17,18 @@ const UserSchema = new mongoose.Schema({
 			/^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/,
 			'Please provide a valid email',
 		],
+		max: [64, 'Email is too long'],
 	},
 	password: {
 		type: String,
 		required: [true, 'Please enter a password'],
-		minlength: 6,
+		min: 6,
+		max: 32,
 		select: false,
 	},
 	resetPasswordToken: String,
 	resetPaswordExpire: Date,
+	chatID: [String],
 });
 
 UserSchema.pre('save', async function (next) {
