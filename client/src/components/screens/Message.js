1866e35392f994cab38c9f47a5e3243adb24d37a
justinasRug <justinas.rugys@gmail.com>
2021-05-11 12:50:02 +0300

    Edit messages(front)

--- client/src/components/screens/Message.js
+++ client/src/components/screens/Message.js
@@ -1,12 +1,14 @@
 import { Card, CardContent, Typography } from '@material-ui/core';
+import { useState, useEffect } from 'react'; 
 import React from 'react';
 import './Message.css';
 
 function Message({ message, username }) {
+	const [edit, setEdit] = useState(false);
+	const [input, setInput] = useState('');
 	const isUser = username === message.username;
 
 	const textToDisplay = () => {
-		console.log(message);
 		if (message.file === '') {
 			if (message.message.includes('giphy')) {
 				return <img src={message.message} alt={''} />;
@@ -18,12 +20,23 @@ function Message({ message, username }) {
 		return 'Something went wrong Message.js';
 	};
 
+	const openEditText =(info) =>{
+		setEdit(edit ?false: true)
+		//console.log(info)
+	}
+	const editText =(info) =>{
+		console.log(info)
+		message.message=input;
+		setEdit(false)
+	}
+
 	return (
 		<div className={`message ${isUser && 'message_user'}`}>
+			{isUser ? <button onClick={() => openEditText(message.message)}>Edit</button> : <></>}
 			<Card className={isUser ? 'message__userCard' : 'message__guestCard'}>
 				<CardContent>
 					<Typography color="initial" varinat="h5" component="h2">
-						{textToDisplay()}
+						{edit ? <div> <input placeholder={message.message} onChange={(e) =>setInput(e.target.value)}></input> <button onClick={() => editText(message.message)}>Change</button> </div> : textToDisplay()}
 					</Typography>
 				</CardContent>
 			</Card>
